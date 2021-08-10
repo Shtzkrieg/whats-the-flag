17e400abda2ad50ebb94c3127e05eb0ae1d247b6
Caleb Hopkins <76622313+Shtzkrieg@users.noreply.github.com>
2021-08-09 21:04:09 -0500

    Create raw_Keras_Image_Rec_Tutorial.py

---
+++ image-rec/dev/raw_Keras_Image_Rec_Tutorial.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import os
+
+
+"""
+This portion removes any corrupted images from the files
+"""
+
+num_skipped = 0
+for folder_name in ("Cat", "Dog"):
+    folder_path = os.path.join("PetImages", folder_name)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = tf.compat.as_bytes("JFIF") in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+print(f"Deleted {num_skipped} images")
+
+"""
+This portion creates the image datasets for training the model and then model validation
+"""
+
+image_size = (180, 180)
+batch_size = 32
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    "PetImages",
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+"""
+This will display 9 random images from the training dataset where dog images are marked with '1' and cats are marked with '0'
+"""
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(int(labels[i]))
+        plt.axis("off")
+        
+""" THIS PORTION IS WHERE I NEED TO FINISH THE REST OF THE FILE AND THEN CHANGE IT TO FLAGS INSTEAD OF PETS """
